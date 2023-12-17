19cafd3373445f8c890946de397e8616a09e6dfd
Chaitanya Modak <86289024+chaitanyamodak@users.noreply.github.com>
2023-12-17 19:47:33 +0530

    Day 47, LeetCode, 242_Valid Anagram

---
+++ 242_Valid Anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count=new int[26];
+        for(char ch:s.toCharArray()){
+            count[ch-'a']++;
+        }
+
+        for(char ch:t.toCharArray()){
+            count[ch-'a']--;
+        }
+
+        boolean allzeros=Arrays.stream(count).allMatch(element->element==0);
+
+        return allzeros;
+    }
+}
