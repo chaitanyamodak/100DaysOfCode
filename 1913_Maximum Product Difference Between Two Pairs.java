e1f9f5f7bc50ff2c7e8ebab085fdae7505240371
Chaitanya Modak <86289024+chaitanyamodak@users.noreply.github.com>
2023-12-18 22:01:37 +0530

    Day 48, LeetCode, 1913_Maximum Product Difference Between Two Pairs

---
+++ 1913_Maximum Product Difference Between Two Pairs.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int maxProductDifference(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        
+        return (nums[n - 1] * nums[n - 2]) - (nums[1] * nums[0]);
+    }
+}
