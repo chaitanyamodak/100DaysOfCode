4a70a4d3ebc436bc73ed3206916ce5dde10c8539
Chaitanya Modak <86289024+chaitanyamodak@users.noreply.github.com>
2024-01-02 23:33:31 +0530

    Day 63, LeetCode, 2610_Convert an Array Into a 2D Array With Conditions

---
+++ 2610_Convert an Array Into a 2D Array With Conditions.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        int mp[]=new int[nums.length+1];
+        List<List<Integer>> result=new ArrayList<>();
+        
+        for(int num:nums){
+            int freq=mp[num];
+
+            if(freq == result.size()){
+                result.add(new ArrayList<>());
+            }
+
+            result.get(freq).add(num);
+            mp[num]++;
+        }
+        return result;
+    }
+}
