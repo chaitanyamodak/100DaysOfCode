d7f69813a8a4dfd29aad6b8bf83d44aa275f0c9f
Chaitanya Modak <86289024+chaitanyamodak@users.noreply.github.com>
2024-01-25 23:44:33 +0530

    Day 86, LeetCode, 1143_Longest Common Subsequence

---
+++ 1143_Longest Common Subsequence.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        
+        int[][] t = new int[m + 1][n + 1];
+        
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (i == 0 || j == 0) {
+                    t[i][j] = 0;
+                } else if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                } else {
+                    t[i][j] = Math.max(t[i][j - 1], t[i - 1][j]);
+                }
+            }
+        }
+        
+        return t[m][n];
+    }
+}
